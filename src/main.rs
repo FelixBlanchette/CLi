f0411745f5c03748336b49645c43ef41a1315dbc
Félix Blanchette <felix.blanchette01@gmail.com>
2025-04-04 14:28:46 -0400

    refactorisation

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    name:String,
+    name: String,
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 }
 
 fn main() {
     let args = Args::parse();
-    for i in 0..args.count{
+    for i in 0..args.count {
         println!("arg: {}, {}", i, args.name);
     }
 }
--- src/utils/log.rs
+++ src/utils/log.rs
@@ -2,10 +2,13 @@ use chrono::Local;
 use std::fs::OpenOptions;
 use std::io::Write;
 
-pub fn log_to_file(message:String, log_file:String)->std::io::Result<()>{
-    let file = OpenOptions::new().create(true).append(true).open(log_file)?;
-    let timestamp=Local::now().format("%Y-%m-%d %H:%M:%S");
-    let log_entry=format!("[{}] {}\n", timestamp, message);
+pub fn log_to_file(message: String, log_file: String) -> std::io::Result<()> {
+    let file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(log_file)?;
+    let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S");
+    let log_entry = format!("[{}] {}\n", timestamp, message);
 
     let mut writer = std::io::BufWriter::new(file);
     writer.write_all(log_entry.as_byte())?;
